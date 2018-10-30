9726d06bd377daf6203b7cfce5bd41a8da4e3d40
Celine Neoh <peiwen124@gmail.com>
2018-10-30 23:48:25 +0800

    First Commit

---
+++ save_image_to_downloads.js
@@ -0,0 +1,38 @@
+function downloadImage(info, tab) {
+	var url = info['srcUrl'];
+	var filename = url.substring(url.lastIndexOf('/')+1);
+	if(chrome.downloads){
+		chrome.downloads.download({ url: url, filename: filename });
+	}
+	else{
+		function getBase64ImageData(url,filename) {
+			var img = new Image();
+			img.setAttribute('crossOrigin', 'anonymous');
+			img.onload = function () {
+				var canvas = document.createElement("canvas");
+				canvas.width = this.naturalWidth;
+				canvas.height = this.naturalHeight;
+				var ctx = canvas.getContext("2d");
+				ctx.drawImage(this, 0, 0);
+				var dataURI = canvas.toDataURL("image/png");
+
+				chrome.tabs.query({currentWindow: true, active: true}, function(tabs) {
+					var tab=tabs[0];
+					chrome.tabs.sendMessage(tab.id, {'action' : 'imagedownload',url:url,filename:filename,dataURI:dataURI},
+						function (response) {
+							if (chrome.runtime.lastError) { 
+								//console.log(chrome.runtime.lastError.message); 
+							} else { 
+								//OK
+							}
+						}
+					);						
+				} );
+				
+			};
+			img.src = url;
+		}
+		getBase64ImageData(url,filename);		
+	}
+}
+chrome.contextMenus.create({"title": "Save Image to Downloads…", "contexts":["image"], "onclick": downloadImage});
